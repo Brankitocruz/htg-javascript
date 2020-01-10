b9f43dae19768376406ddd60b70e937a81351e26
Brankitocruz <brankocruz15@gmail.com>
2020-01-10 17:49:26 -0300

    ejercicio 2

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,17 @@
 console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+
+const comentarios = document.querySelectorAll("div");
+
+
+comentarios[2].classList.add("commentAgus");
+
+/*E2*/
+
+let hr = document.querySelectorAll("hr");
+
+for(let i= 0;i<hr.length; i++){
+    hr[i].classList.add("banana");
+}
+
+
+
